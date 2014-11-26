476efabe8ecd742f5148dc11a5cf7033f1c8727b
Ariel Herrera <arielhc5@gmail.com>
2014-11-25 20:22:05 -0600

    Login y pantalla pedidos
    Este cambio muestra la funcionalidad del login y si lo valida muestra la
    pantalla de una lista de pedidos

--- application/controllers/master.php
+++ application/controllers/master.php
@@ -1,36 +1,64 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
 class Master extends CI_Controller {
-    
-    public function __construct() {
+
+    public function __construct()
+    {
         parent::__construct();
-    }
-    
-    public function index() {        
-        //Se define que vista
-        $data['main']='signin';
-        //Se preparan los parametros de la vista
-        $data['title']='Altuve | Iniciar sesión';
-        //Se carga la vista con plantilla
-        $this->load->view('includes/template',$data);
+        $this->load->model('Login_Model');
     }
 
-    public function registro() {        
-        //Se define que vista
-        $data['main']='signup';
-        //Se preparan los parametros de la vista
-        $data['title']='Registro de usuario | Altuve';
-        //Se carga la vista con plantilla
-        $this->load->view('includes/template',$data);
+    public function index() {
+        {  // reglas de validación
+        $this->form_validation->set_rules('nombre', 'Usuario', 'required|xss_clean|callback__valid_login');
+        $this->form_validation->set_rules('password', 'Contraseña','required|xss_clean');
+	   
+	$this->form_validation->set_message('required', 'el campo %s es requerido');
+        $this->form_validation->set_message('_valid_login', 'El usuario o contraseña son incorrectos');
+	   
+	$this -> form_validation -> set_error_delimiters('<ul><li>', '</li></ul>');
+     
+		if ($this->form_validation->run() == FALSE){
+                    //Se define que vista
+                    $data['main'] = 'signin';
+                    //Se preparan los parametros de la vista
+                    $data['title'] = 'Altuve | Iniciar sesión';
+                    //Se carga la vista con plantilla
+                    $this->load->view('includes/template', $data);
+        }
+        else
+                {
+                $username = $this->input->post('username');
+                             $data_user = $array = array('user'=> $username, 'logued_in' => TRUE);
+
+                // asignamos dos datos a la sesión --> (username y logued_in)									 
+                $this->session->set_userdata($data_user);
+                //Se define que vista
+                    $data['main'] = 'pedidos';
+                    //Se preparan los parametros de la vista
+                    $data['title'] = 'Altuve | Bienvenido';
+                    //Se carga la vista con plantilla
+                    $this->load->view('includes/template', $data);
+                }
+        }
     }
-    
-    public function plantilla (){
+
+    public function registro() {
         //Se define que vista
-        $data['main']='plantilla';
+        $data['main'] = 'signup';
         //Se preparan los parametros de la vista
-        $data['title']='Altuve | Iniciar sesión';
+        $data['title'] = 'Registro de usuario | Altuve';
         //Se carga la vista con plantilla
-        $this->load->view('includes/template',$data);
+        $this->load->view('includes/template', $data);
+    }
+
+    public function plantilla() {
+        //Se define que vista
+        $data['main'] = 'plantilla';
+        //Se preparan los parametros de la vista
+        $data['title'] = 'Altuve | Iniciar sesión';
+        //Se carga la vista con plantilla
+        $this->load->view('includes/template', $data);
     }
 
 }
---
+++ application/views/pedidos.php
@@ -0,0 +1,143 @@
+	<body class="tooltips">
+		<div class="wrapper">			
+			<div class="top-navbar">
+				<div class="top-navbar-inner">
+					<div class="logo-brand">
+						<a href="#"><img src="assets/img/sentir-logo-primary.png" alt="Sentir logo"></a>
+					</div><!-- /.logo-brand -->
+					<!-- End Logo brand -->
+				</div><!-- /.top-navbar-inner -->
+			</div><!-- /.top-navbar -->
+			<!-- END TOP NAV -->
+                        
+			<!-- BEGIN PAGE CONTENT -->
+			<div class="page-content">
+				
+				
+				<div class="container-fluid">
+					<!-- Begin page heading -->
+					<h1 class="page-heading">Orden de pedidos</h1>
+					<!-- End page heading -->
+				
+					<!-- Begin breadcrumb -->
+					
+					<div class="the-box full no-border">
+					<div class="table-responsive">
+						<table class="table table-th-block table-success">
+							<thead>
+								<tr>
+									<th style="width: 100px;">Orden ID</th>
+									<th>Fecha de orden</th>
+									<th>Forma de pago</th>
+									<th>Total orden</th>
+									<th>Status</th>
+									<th>Detalle</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td><strong>#SNT0032</strong></td>
+									<td><span class="span span-info">May 01, 2014</span></td>
+									<td>Paypal</td>
+									<td><strong>&#36; 165.00</strong></td>
+									<td><span class="label label-warning">Pending Payment</span></td>
+									<td><button class="btn btn-default btn-xs"><i class="fa fa-share"></i></button></td>
+								</tr>
+								<tr>
+									<td><strong>#SNT0024</strong></td>
+									<td><span class="span span-info">May 01, 2014</span></td>
+									<td>Credit Card</td>
+									<td><strong>&#36; 175.00</strong></td>
+									<td><span class="label label-success">Completed</span></td>
+									<td><button class="btn btn-default btn-xs"><i class="fa fa-share"></i></button></td>
+								</tr>
+								<tr>
+									<td><strong>#SNT0021</strong></td>
+									<td><span class="span span-info">May 01, 2014</span></td>
+									<td>Paypal</td>
+									<td><strong>&#36; 165.00</strong></td>
+									<td><span class="label label-success">Completed</span></td>
+									<td><button class="btn btn-default btn-xs"><i class="fa fa-share"></i></button></td>
+								</tr>
+								<tr>
+									<td><strong>#SNT0015</strong></td>
+									<td><span class="span span-info">May 01, 2014</span></td>
+									<td>Paypal</td>
+									<td><strong>&#36; 165.00</strong></td>
+									<td><span class="label label-danger">Canceled</span></td>
+									<td><button class="btn btn-default btn-xs"><i class="fa fa-share"></i></button></td>
+								</tr>
+								<tr>
+									<td><strong>#SNT0011</strong></td>
+									<td><span class="span span-info">May 01, 2014</span></td>
+									<td>Paypal</td>
+									<td><strong>&#36; 165.00</strong></td>
+									<td><span class="label label-success">Completed</span></td>
+									<td><button class="btn btn-default btn-xs"><i class="fa fa-share"></i></button></td>
+								</tr>
+								<tr>
+									<td><strong>#SNT0009</strong></td>
+									<td><span class="span span-info">May 01, 2014</span></td>
+									<td>Paypal</td>
+									<td><strong>&#36; 165.00</strong></td>
+									<td><span class="label label-success">Completed</span></td>
+									<td><button class="btn btn-default btn-xs"><i class="fa fa-share"></i></button></td>
+								</tr>
+								<tr>
+									<td><strong>#SNT0032</strong></td>
+									<td><span class="span span-info">May 01, 2014</span></td>
+									<td>Paypal</td>
+									<td><strong>&#36; 165.00</strong></td>
+									<td><span class="label label-warning">Pending Payment</span></td>
+									<td><button class="btn btn-default btn-xs"><i class="fa fa-share"></i></button></td>
+								</tr>
+								<tr>
+									<td><strong>#SNT0024</strong></td>
+									<td><span class="span span-info">May 01, 2014</span></td>
+									<td>Credit Card</td>
+									<td><strong>&#36; 175.00</strong></td>
+									<td><span class="label label-success">Completed</span></td>
+									<td><button class="btn btn-default btn-xs"><i class="fa fa-share"></i></button></td>
+								</tr>
+								<tr>
+									<td><strong>#SNT0021</strong></td>
+									<td><span class="span span-info">May 01, 2014</span></td>
+									<td>Paypal</td>
+									<td><strong>&#36; 165.00</strong></td>
+									<td><span class="label label-success">Completed</span></td>
+									<td><button class="btn btn-default btn-xs"><i class="fa fa-share"></i></button></td>
+								</tr>
+								<tr>
+									<td><strong>#SNT0015</strong></td>
+									<td><span class="span span-info">May 01, 2014</span></td>
+									<td>Paypal</td>
+									<td><strong>&#36; 165.00</strong></td>
+									<td><span class="label label-danger">Canceled</span></td>
+									<td><button class="btn btn-default btn-xs"><i class="fa fa-share"></i></button></td>
+								</tr>
+								<tr>
+									<td><strong>#SNT0011</strong></td>
+									<td><span class="span span-info">May 01, 2014</span></td>
+									<td>Paypal</td>
+									<td><strong>&#36; 165.00</strong></td>
+									<td><span class="label label-success">Completed</span></td>
+									<td><button class="btn btn-default btn-xs"><i class="fa fa-share"></i></button></td>
+								</tr>
+								<tr>
+									<td><strong>#SNT0009</strong></td>
+									<td><span class="span span-info">May 01, 2014</span></td>
+									<td>Paypal</td>
+									<td><strong>&#36; 165.00</strong></td>
+									<td><span class="label label-success">Completed</span></td>
+									<td><button class="btn btn-default btn-xs"><i class="fa fa-share"></i></button></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+					</div>
+					
+					
+				
+				</div>
+	</body>
+</html>
