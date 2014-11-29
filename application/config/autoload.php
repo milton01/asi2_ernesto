6023b85dfb26e9ec9075c980e985806e8a6c54d2
milton hernandez <mrhdiaz@gmail.com>
2014-11-28 23:44:30 -0600

    database

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -1,21 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /*
 | -------------------------------------------------------------------
-| AUTO-LOADER
-| -------------------------------------------------------------------
-| This file specifies which systems should be loaded by default.
-|
-| In order to keep the framework as light-weight as possible only the
-| absolute minimal resources are loaded by default. For example,
-| the database is not connected to automatically since no assumption
-| is made regarding whether you intend to use it.  This file lets
-| you globally define which systems you would like loaded with every
-| request.
-|
-| -------------------------------------------------------------------
-| Instructions
-| -------------------------------------------------------------------
-|
+
 | These are the things you can load automatically:
 |
 | 1. Packages
